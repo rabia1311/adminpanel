14cb5c0b8cda32a548b0b0b27261def9f02a6e3a
rabia1311 <rabiasultana.n@gmail.com>
2023-06-25 01:54:19 +1030

    error resolved

--- adminbackend/controllers/category.controller.js
+++ adminbackend/controllers/category.controller.js
@@ -1,14 +1,27 @@
-const Category=require("../Models/Category");
+const Category = require("../Models/Category");
+
 const createCategory = async (req, res) => {
-    try {
-        const image = req.file.filename;
-        const newCategory=new Category({
-            ...req.body,
-            image,
-        });
-        const savedCategory = await newCategory.save();
-        res.status(201).json({
-            success: true,
-            message: "Category added successfully.",
-            data: savedCategory,
-        })
+  try {
+    const image = req.file.filename;
+    const newCategory = new Category({
+      ...req.body,
+      image,
+    });
+    const savedCategory = await newCategory.save();
+    res.status(201).json({
+      success: true,
+      message: "Category added successfully.",
+      data: savedCategory,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Failed to add category.",
+      error: error.message,
+    });
+  }
+};
+
+module.exports = {
+  createCategory,
+};
